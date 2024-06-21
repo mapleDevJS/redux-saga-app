70f4c63bde5a8304821638f041f1a2478355517c
Alexey Ivanov <alexey.ivanov.js@gmail.com>
2024-06-21 11:27:07 -0400

    Refactor and improve Users component
    The UsersComponent file has been deleted and replaced with a more streamlined Users component in a new file. The component's state is now destructured for easier readability and use, and the rendering logic has been abstracted into a separate function to tidy up the return statement. The App component has been updated to import from this new Users file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Users from './components/UsersComponent';
+import Users from './components/Users.jsx';
 import './App.css';
 
 function App() {
---
+++ src/components/Users.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { getUsers } from '../redux/actions/users';
+import Card from './CardComponent';
+
+const Users = () => {
+  const dispatch = useDispatch();
+  const { users, loading, error } = useSelector(state => state.users);
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
+  const isLoading = loading;
+  const noUsersAvailable = users.length === 0 && !loading;
+
+  const renderContent = () => {
+    if (isLoading) return <p>Loading...</p>;
+    if (noUsersAvailable) return <p>No users available!</p>;
+    if (error && !loading) return <p>{error}</p>;
+    return users.map((user) => <Card key={user.id} user={user} />);
+  };
+
+  return (
+      <>
+        {renderContent()}
+      </>
+  );
+}
+
+export default Users;
